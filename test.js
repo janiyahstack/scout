f94e23b4ffafba936249e910d365230ec3e63929
Zhul <suliyatkataye2@gmail.com>
2026-09-23 12:17:30 +0100

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import OpenAI from "openai";
 const app = express();
 app.use(cors());
-app.use|(express.json());
+app.use(express.json());
 
 const client = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
